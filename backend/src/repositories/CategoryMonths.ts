854d0c11097f9e17f0c501c41e3cc67e90dc3903
Alex Phillips <ahp118@gmail.com>
2021-12-28 16:11:26 -0500

    refactored findOrCreate to use create individually

--- backend/src/repositories/CategoryMonths.ts
+++ backend/src/repositories/CategoryMonths.ts
@@ -6,6 +6,22 @@ import { BudgetMonths } from "./BudgetMonths";
 
 @EntityRepository(CategoryMonth)
 export class CategoryMonths extends Repository<CategoryMonth> {
+  async createNew(budgetId: string, categoryId: string, month: string): Promise<CategoryMonth> {
+    const budgetMonth = await this.manager.getCustomRepository(BudgetMonths).findOrCreate(budgetId, month)
+    const categoryMonth = this.create({
+      budgetMonthId: budgetMonth.id,
+      categoryId,
+      month: month,
+      // @TODO: I DON'T KNOW WHY I HAVE TO SPECIFY 0s HERE AND NOT ABOVE WHEN CREATING BUDGET MONTH!!! AHHH!!!
+      activity: dinero({ amount: 0, currency: USD }),
+      balance: dinero({ amount: 0, currency: USD }),
+      budgeted: dinero({ amount: 0, currency: USD }),
+    })
+    categoryMonth.budgetMonth = Promise.resolve(budgetMonth)
+
+    return categoryMonth
+  }
+
   async findOrCreate(budgetId: string, categoryId: string, month: string): Promise<CategoryMonth> {
     let categoryMonth: CategoryMonth = await this.findOne(
       { categoryId, month: month },
@@ -13,18 +29,8 @@ export class CategoryMonths extends Repository<CategoryMonth> {
     )
 
     if (!categoryMonth) {
-      const budgetMonth = await this.manager.getCustomRepository(BudgetMonths).findOrCreate(budgetId, month)
-      categoryMonth = this.create({
-        budgetMonthId: budgetMonth.id,
-        categoryId,
-        month: month,
-        // @TODO: I DON'T KNOW WHY I HAVE TO SPECIFY 0s HERE AND NOT ABOVE WHEN CREATING BUDGET MONTH!!! AHHH!!!
-        activity: dinero({ amount: 0, currency: USD }),
-        balance: dinero({ amount: 0, currency: USD }),
-        budgeted: dinero({ amount: 0, currency: USD }),
-      })
+      categoryMonth = await this.createNew(budgetId, categoryId, month)
       await this.insert(categoryMonth)
-      categoryMonth.budgetMonth = Promise.resolve(budgetMonth)
     }
 
     return categoryMonth
