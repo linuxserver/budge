dd888d2a9d000a34080f38ea8673163aabd3349f
Alex Phillips <ahp118@gmail.com>
2022-01-17 15:14:38 -0500

    missed a dep

--- frontend/src/utils/Store.js
+++ frontend/src/utils/Store.js
@@ -1,4 +1,5 @@
 import { createSelectorCreator, defaultMemoize } from 'reselect'
+import _ from 'lodash'
 
 // create a "selector creator" that uses lodash.isequal instead of ===
 export const createDeepEqualSelector = createSelectorCreator(defaultMemoize, _.isEqual)
