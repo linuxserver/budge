6d5b791c51e75883dc622eb124de5524fe72b1c4
Alex Phillips <ahp118@gmail.com>
2022-02-07 19:22:35 -0500

    if account doesn't exist, redirect to budget page

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,12 +1,23 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import AccountTable from '../components/AccountTable/AccountTable'
+import { useNavigate } from 'react-router-dom'
+import { accountsSelectors } from '../redux/slices/Accounts'
 
 export default function Account(props) {
+  const navigate = useNavigate()
+
   const params = useParams()
+  const account = useSelector(state => accountsSelectors.selectById(state, params.accountId))
+
+  useEffect(() => {
+    if (!account) {
+      navigate('/')
+    }
+  }, [])
 
   return (
-    <div style={{ maxWidth: '100%' }}>
-      <AccountTable accountId={params.accountId} />
-    </div>
+    <div style={{ maxWidth: '100%' }}>{account && <AccountTable accountId={params.accountId} account={account} />}</div>
   )
 }
