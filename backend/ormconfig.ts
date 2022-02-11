40cfa39310e50f355a9bd019db4d435a835d216d
Alex Phillips <ahp118@gmail.com>
2022-02-10 20:39:43 -0500

    attempting to fix prod pathing

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,19 +1,35 @@
-import { ConnectionOptions } from "typeorm"
+import { ConnectionOptions } from 'typeorm'
 
 const { join } = require('path')
 
 const config: ConnectionOptions = {
   type: 'sqlite',
-  // host: process.env.BUDGE_DB_HOST || 'localhost',
-  // port: parseInt(process.env.BUDGE_DB_PORT) || 3306,
-  // username: process.env.BUDGE_DB_USERNAME,
-  // password: process.env.BUDGE_DB_PASSWORD,
   database: process.env.BUDGE_DATABASE || './budge.sqlite',
+
+  // type: 'postgres',
+  // port: parseInt(process.env.BUDGE_DB_PORT) || 5432,
+
+  // type: 'mariadb',
+  // port: parseInt(process.env.BUDGE_DB_PORT) || 3306,
+
+  // host: process.env.BUDGE_DB_HOST || '192.168.1.91',
+  // username: process.env.BUDGE_DB_USERNAME || 'budge',
+  // password: process.env.BUDGE_DB_PASSWORD || '3uY8xTX6j8oTVqJ_',
+  // database: process.env.BUDGE_DATABASE || 'budge',
   synchronize: true,
   logging: false,
-  entities: [join(__dirname, 'src/entities/**', '*.{ts,js}')],
-  migrations: [join(__dirname, 'src/migrations/**', '*.{ts,js}')],
-  subscribers: [join(__dirname, 'src/subscribers/**', '*.{ts,js}')],
+  entities: [
+    join(__dirname, 'src/entities/**', '*.ts'),
+    join(__dirname, 'build/src/entities/**', '*.js')
+  ],
+  migrations: [
+    join(__dirname, 'src/migrations/**', '*.ts'),
+    join(__dirname, 'build/src/migrations/**', '*.js')
+  ],
+  subscribers: [
+    join(__dirname, 'src/subscribers/**', '*.ts'),
+    join(__dirname, 'build/src/subscribers/**', '*.js')
+  ],
   cli: {
     entitiesDir: 'src/entities',
     migrationsDir: 'src/migrations',
