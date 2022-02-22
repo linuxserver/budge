79b2da18afa292b1555a6f502008c70f5845eaee
Alex Phillips <ahp118@gmail.com>
2022-02-22 09:38:09 -0500

    using luxon for datetimes to keep consistency when generating dates across timezones

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,42 +1,39 @@
+import { DateTime } from 'luxon'
+
 export async function sleep(duration: number): Promise<void> {
   return new Promise(resolve => {
     setTimeout(resolve, duration)
   })
 }
 
-export function getDateFromString(date: string): Date {
-  let [year, month, day] = date.split('-').map(val => parseInt(val))
-
-  // Month is zero-indexed in JS...
-  month = month - 1
-  if (month < 0) {
-    month = 11
-    year = year - 1
-  }
-
-  return new Date(year, month, day)
+export function getDateFromString(date: string): DateTime {
+  return DateTime.fromISO(date)
 }
 
 export function formatMonthFromDateString(date: Date): string {
-  let tempDate = new Date(date)
-  tempDate.setHours(0)
-  tempDate.setDate(1)
-  return tempDate.toISOString().split('T')[0]
+  let tempDate = DateTime.fromISO(date.toISOString())
+  tempDate = tempDate.set({ day: 1 })
+  return tempDate.toISO().split('T')[0]
 }
 
-export function getMonthDate(): Date {
-  const today = new Date()
-  const date = new Date(today.getFullYear(), today.getMonth(), 1)
-  return date
+export function getMonthDate(): DateTime {
+  const today = DateTime.now()
+  return today.set({ day: 1 })
 }
 
 export function getMonthString(): string {
-  return getMonthDate().toISOString().split('T')[0]
+  return getMonthDate().toISO().split('T')[0]
 }
 
 export function getMonthStringFromNow(monthsAway: number): string {
-  const today = new Date()
-  const date = new Date(today.getFullYear(), today.getMonth(), 1)
-  date.setMonth(date.getMonth() + monthsAway)
-  return date.toISOString().split('T')[0]
+  let today: DateTime = DateTime.now()
+  today = today.set({ day: 1 })
+
+  if (monthsAway > 0) {
+    today = today.plus({ month: monthsAway })
+  } else {
+    today = today.minus({ month: monthsAway })
+  }
+
+  return today.toISO().split('T')[0]
 }
