208a37c971e9646305efaadf4d0e0b59eed92604
Alex Phillips <ahp118@gmail.com>
2022-04-05 09:22:49 -0400

    changing from typeorm sync to migrations - initial migration checks for existing user table and skips migration if it exists as this was a DB already generated by the sync

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -5,18 +5,7 @@ const { join } = require('path')
 const config: ConnectionOptions = {
   type: 'sqlite',
   database: process.env.BUDGE_DATABASE || './budge.sqlite',
-
-  // type: 'postgres',
-  // port: parseInt(process.env.BUDGE_DB_PORT) || 5432,
-
-  // type: 'mariadb',
-  // port: parseInt(process.env.BUDGE_DB_PORT) || 3306,
-
-  // host: process.env.BUDGE_DB_HOST || '192.168.1.91',
-  // username: process.env.BUDGE_DB_USERNAME || 'budge',
-  // password: process.env.BUDGE_DB_PASSWORD || '3uY8xTX6j8oTVqJ_',
-  // database: process.env.BUDGE_DATABASE || 'budge',
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [join(__dirname, 'src/entities/**', '*.{ts,js}')],
   migrations: [join(__dirname, 'src/migrations/**', '*.{ts,js}')],
